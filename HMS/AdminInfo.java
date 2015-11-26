90a7a1b6c55e9e57d8afc2d70e98007770701da4
pefido <pedrofilipeduraes@hotmail.com>
2015-11-26 14:23:53 +0000

    modify internalInfo and adminInfo

--- HMS/AdminInfo.java
+++ HMS/AdminInfo.java
@@ -2,4 +2,17 @@ package HMS;
 
 public class AdminInfo {
 
+  private boolean complaint;
+
+  public AdminInfo(boolean complaint){
+    this.complaint = complaint;
+  }
+
+  public boolean getComplaint(){
+    return complaint;
+  }
+
+  public void setComplaint(boolean complaint){
+    this.complaint = complaint;
+  }
 }
--- HMS/InternalInfo.java
+++ HMS/InternalInfo.java
@@ -2,4 +2,27 @@ package HMS;
 
 public class InternalInfo {
 
+  private int expense;
+  private int income;
+
+  public InternalInfo(int expense, int income){
+    this.expense = expense;
+    this.income = income;
+  }
+
+  public int getExpense(){
+  	return expense;
+  }
+
+  public void setExpense(int expense){
+  	this.expense = expense;
+  }
+
+  public int getIncome(){
+  	return income;
+  }
+
+  public void setIncome(int income){
+  	this.income = income;
+  }
 }
